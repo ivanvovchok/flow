e1d772a804ba158cfc96505fcf29742b388e07b6
Ivan Vovchok <elvovchok@gmail.com>
2022-10-22 18:30:44 +0300

    feat: add preloader component

---
+++ src/components/Layout/Preloader/Preloader.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { SyncOutlined } from "@ant-design/icons";
+
+const Preloader: React.FC = () => {
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: 'calc(100vh - 32px)',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 28,
+        fontWeight: 200,
+        color: "white",
+      }}
+    >
+      FL<SyncOutlined spin />W
+    </div>
+  )
+}
+
+export default Preloader
