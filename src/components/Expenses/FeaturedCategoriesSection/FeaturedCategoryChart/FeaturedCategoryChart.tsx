501f6b8b02c6ee4c732381466d693aa0213479b2
Ivan Vovchok <elvovchok@gmail.com>
2022-10-22 21:36:49 +0300

    feat: add featured categories section component

---
+++ src/components/Expenses/FeaturedCategoriesSection/FeaturedCategoryChart/FeaturedCategoryChart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { TinyArea } from '@ant-design/plots';
+import useFeaturedCategoryChart from "./useFeaturedCategoryChart";
+
+const FeaturedCategoryChart: React.FC = () => {
+  const { config } = useFeaturedCategoryChart();
+
+  return(
+    <TinyArea {...config} />
+  )
+}
+
+export default FeaturedCategoryChart
---
+++ src/components/Expenses/FeaturedCategoriesSection/FeaturedCategorySection.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Row, Col, Divider, Tag } from 'antd';
+import FeaturedCategoryChart from "./FeaturedCategoryChart";
+
+const FeaturedCategorySection: React.FC = () => {
+  return(
+    <>
+      <Divider>Вибрані категорії:</Divider>
+      <Row gutter={24} justify={"space-evenly"}  style={{ marginTop: 20 }}>
+        <Col span={7}>
+          <Tag color="#108ee9" style={{ marginBottom: 10 }}>Їжа</Tag>
+          <FeaturedCategoryChart />
+        </Col>
+        <Col span={7}>
+          <Tag color="#2db7f5" style={{ marginBottom: 10 }}>Розваги</Tag>
+          <FeaturedCategoryChart />
+        </Col>
+        <Col span={7}>
+          <Tag color="#87d068" style={{ marginBottom: 10 }}>Транспорт</Tag>
+          <FeaturedCategoryChart />
+        </Col>
+      </Row>
+    </>
+
+  );
+}
+
+export default FeaturedCategorySection
