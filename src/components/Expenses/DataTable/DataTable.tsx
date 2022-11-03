34820acaaaec2450ee4de7a39eb963dc21504908
Ivan Vovchok <elvovchok@gmail.com>
2022-11-03 09:28:34 +0200

    feat: add search input for data table

--- src/components/Expenses/DataTable/DataTable.tsx
+++ src/components/Expenses/DataTable/DataTable.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import {Table} from 'antd';
+import {Table, Divider, Input, Row} from 'antd';
 import useDataTable from "./useDataTable";
 
-const DataTable: React.FC = () => {
+const {Search} = Input;
+
+const DataTable: React.FC = (): JSX.Element => {
   const {
     data,
     columns
-  } = useDataTable()
+  } = useDataTable();
 
   return (
-    <Table dataSource={data} columns={columns}/>
+    <>
+      <Divider
+        style={{
+          marginBottom: 20
+        }}
+      >Історія витрат:</Divider>
+      <Row justify={'end'}>
+        <Search
+          enterButton
+          style={{
+            width: 240,
+            marginBottom: 20
+          }} />
+      </Row>
+      <Table
+        dataSource={data}
+        columns={columns}
+        pagination={{
+          hideOnSinglePage: true,
+          pageSize:10,
+
+        }}
+      />
+    </>
   )
 }
 
